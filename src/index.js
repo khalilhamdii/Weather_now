daf6a444beefe88bebfa0fb0adc5b2f35804f64d
khalilhamdii <khaalil.hamdi@gmail.com>
2020-12-22 15:38:56 +0100

    Add background method

--- src/index.js
+++ src/index.js
@@ -1,13 +1,22 @@
 // import start
 import Weather from "./assets/js/weather";
-
+import bgImg from "./assets/img/background.jpg";
 const importAll = (r) => r.keys().map(r);
 importAll(require.context("./assets/js/", true, /\.js$/));
 importAll(require.context("./assets/css/", true, /\.css$/));
 
 // import end
 
+const bodyBg = (img) => {
+  document.body.style.background = `linear-gradient(
+    90deg,
+    rgba(58, 65, 250, 0.5),
+    rgba(255, 255, 255, 0.5)
+  ), url(${img}) top no-repeat`;
+};
+
 document.addEventListener("DOMContentLoaded", () => {
+  bodyBg(bgImg);
   const searchBtn = document.getElementById("search-btn");
   const inputToggle = document.getElementById("unit-check");
 
