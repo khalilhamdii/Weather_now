e5c5f0b3a4dc9584781e647de087ddbc8afcd10b
khalilhamdii <khaalil.hamdi@gmail.com>
2020-12-22 13:49:07 +0100

    Fix toggle issue as requested by TSE

--- src/index.js
+++ src/index.js
@@ -1,17 +1,31 @@
 // import start
-import Weather from './assets/js/weather';
+import Weather from "./assets/js/weather";
 
 const importAll = (r) => r.keys().map(r);
-importAll(require.context('./assets/js/', true, /\.js$/));
-importAll(require.context('./assets/css/', true, /\.css$/));
+importAll(require.context("./assets/js/", true, /\.js$/));
+importAll(require.context("./assets/css/", true, /\.css$/));
 
 // import end
 
-const searchBtn = document.getElementById('search-btn');
-searchBtn.addEventListener('click', () => {
-  const city = document.querySelector('input').value;
-  const unit = Weather.getUnit();
-  Weather.resetData();
-  Weather.renderLoader(true);
-  Weather.getCityData(city, unit);
+document.addEventListener("DOMContentLoaded", () => {
+  const searchBtn = document.getElementById("search-btn");
+  const inputToggle = document.getElementById("unit-check");
+
+  searchBtn.addEventListener("click", () => {
+    const city = document.querySelector("input").value;
+    const unit = Weather.getUnit();
+    Weather.resetData();
+    Weather.renderLoader(true);
+    Weather.getCityData(city, unit);
+  });
+
+  inputToggle.addEventListener("click", () => {
+    const city = document.querySelector("input").value;
+    const unit = Weather.getUnit();
+    if (city.length > 0) {
+      Weather.resetData();
+      Weather.renderLoader(true);
+      Weather.getCityData(city, unit);
+    }
+  });
 });
